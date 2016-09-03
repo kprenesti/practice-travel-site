18bfe93a91ef261ef5ecea46a9ca987eb5dd66db
Krista Prenesti <kprenesti@gmail.com>
2016-09-02 22:12:58 -0400

    Finished our story section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     imports = require('postcss-import'),
+    hexrgba = require('postcss-hexrgba'),
     mixins = require('postcss-mixins');
 
 gulp.task('css', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([imports, mixins, cssvars, autoprefixer, nested]))
+  .pipe(postcss([imports, mixins, cssvars, autoprefixer, hexrgba, nested]))
   .on('error', function(error){
     console.log(error.toString());
     this.emit('end');
