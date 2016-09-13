5ff4f0ec12cfd4f9ce98435ea8b3eb80e15f264a
Krista Prenesti <kprenesti@gmail.com>
2016-09-13 15:23:27 -0400

    finished mobile menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,10 +2,7 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create();
 
-    gulp.task('cssInject', ['css'], function(){
-      return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
-    });
+
 
     gulp.task('watch', function(){
 
@@ -17,12 +14,21 @@ var gulp = require('gulp'),
 
       watch('./app/index.html', function(){
         browserSync.reload();
-      })
+      });
       watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
       });
-
-      gulp.task('default', function(){
-        console.log('You need to add stuff to the default task!');
+      watch('./app/assets/scripts/**/*.js', function(){
+        gulp.start('scriptsInject');
       });
+    });
+
+
+    gulp.task('cssInject', ['css'], function(){
+      return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+    });
+
+    gulp.task('scriptsInject', ['scripts'], function(){
+      browserSync.reload();
     });
