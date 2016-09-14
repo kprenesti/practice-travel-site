872cbca63764fd77e768ee9a5e52bb85c7828261
Krista Prenesti <kprenesti@gmail.com>
2016-09-14 11:18:19 -0400

    Created modal and styled social icons

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -15,9 +15,11 @@ var gulp = require('gulp'),
       watch('./app/index.html', function(){
         browserSync.reload();
       });
+
       watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
       });
+      
       watch('./app/assets/scripts/**/*.js', function(){
         gulp.start('scriptsInject');
       });
