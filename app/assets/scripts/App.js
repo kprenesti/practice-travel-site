a46f5d6101cac5dd16b497f2adbe809d62629350
Krista Prenesti <kprenesti@gmail.com>
2016-09-13 21:16:36 -0400

    Created RevealOnScroll module

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,4 @@
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
